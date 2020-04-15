523388b6471d4178f37c4ac745ad50a7534f45d0
Migueljfc <miguel.f.cabral@gmail.com>
2020-04-15 19:10:18 +0100

    tp07 complete

--- tp07/p2_3.c
+++ tp07/p2_3.c
@@ -12,7 +12,7 @@ void _int_(12) isr_T3(void){
 void main(void){
     TRISDbits.TRISD0=0;
 
-    T3CONbits.TCKPS = 5; // 1:32 prescaler (i.e Fout_presc = 625 KHz)
+    T3CONbits.TCKPS = 0x2; // 1:32 prescaler (i.e Fout_presc = 625 KHz)
     PR3 = 49999; 
     TMR3 = 0; // Reset timer T3 count register
     T3CONbits.TON = 1; // Enable timer T3 (must be the last command of the
